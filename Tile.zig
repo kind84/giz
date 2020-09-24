a77fa1ac4e5ab1681784e5f3d420ee5ac68ad783
Paolo Grisoli <paolo_g84@icloud.com>
2020-09-24 08:27:04 +0200

    full tile downscaling

--- Tile.zig
+++ Tile.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const mem = std.mem;
 const Allocator = mem.Allocator;
 
+pub const io_mode = .evented;
+
 const OddSide = enum(u3) {
     None = 1,
     Height = 2,
@@ -45,7 +47,7 @@ x: u32,
 y: u32,
 h: u32,
 w: u32,
-channels: [][]u8,
+channels: [][]const u8,
 
 pub fn init(idx: u32, x: u32, y: u32, h: u32, w: u32, chans: [][]u8) *Tile {
     return &Tile{
@@ -59,10 +61,10 @@ pub fn init(idx: u32, x: u32, y: u32, h: u32, w: u32, chans: [][]u8) *Tile {
 }
 
 pub fn downscale(self: Tile) !*Tile {
-    const allocator = std.heap.page_allocator;
-    const FrameType = @TypeOf(async downsample([]const u8, u32, u32));
-    var frames = try allocator.alloc(FrameType, self.channels.len);
-    var down: [self.channels.len][]u8 = undefined;
+    const FrameType = @TypeOf(async downsample(self.channels[0], self.h, self.w));
+    var frames = try self.allocator.alloc(FrameType, self.channels.len);
+    defer self.allocator.free(frames);
+    var down = try self.allocator.alloc([]u8, self.channels.len);
 
     for (self.channels) |c, i| {
         frames[i] = async downsample(c, self.h, self.w);
@@ -148,6 +150,28 @@ fn downsample(chan: []const u8, height: u32, width: u32) ![]u8 {
     return buff_down;
 }
 
+test "downscale" {
+    const chan = [_]u8{8} ** 64;
+    var channels = [_][]const u8{chan[0..]} ** 3;
+
+    var t = Tile{
+        .allocator = std.heap.page_allocator,
+        .index = 1,
+        .x = 0,
+        .y = 0,
+        .h = 8,
+        .w = 8,
+        .channels = &channels,
+    };
+
+    var dt = try t.downscale();
+    defer t.allocator.free(dt.channels);
+
+    std.debug.assert(dt.channels.len == 3);
+    std.debug.assert(dt.channels[0].len == 16);
+    std.debug.assert(dt.channels[0][0] == 8);
+}
+
 test "downsample" {
     const tests = [_]struct {
         chan: []const u8,
@@ -184,6 +208,18 @@ test "downsample" {
             .exp_len = 15,
             .height = 9,
             .width = 6,
+        },
+        .{
+            .chan = &[_]u8{8} ** 6,
+            .exp_len = 2,
+            .height = 2,
+            .width = 3,
+        },
+        .{
+            .chan = &[_]u8{8} ** 2,
+            .exp_len = 1,
+            .height = 2,
+            .width = 1,
         },
     };
 
